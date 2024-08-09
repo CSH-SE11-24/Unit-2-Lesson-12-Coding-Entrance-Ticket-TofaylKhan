d5353a534843f01209c66844be3b330854b3e71e
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 21:00:43 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Prompt the user for a number and cast it to a number [1.2 - 2 pts]
+let num = prompt("give a number") 
+num = parseInt(num)
+
+
+
+
+// Write a JavaScript program that uses a for loop to print all odd numbers from 1 to the number the user typed in [2.3 - 4 pts]
+for (let i = 1; i <= num; i+=2){
+  console.log(i)
+}
+
+
+
+
+
